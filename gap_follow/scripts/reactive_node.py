fc24b9e95a1179387c46209d4fc95dc1d74fa625
Mohamed Mahmoud <mohamednessim15@gmail.com>
2026-02-03 14:48:17 +0200

    modified:   gap_follow/scripts/reactive_node.py

--- gap_follow/scripts/reactive_node.py
+++ gap_follow/scripts/reactive_node.py
@@ -15,10 +15,15 @@ class ReactiveFollowGap(Node):
         # Topics & Subs, Pubs
         lidarscan_topic = '/scan'
         drive_topic = '/drive'
+        self.car_width = 0.28
+        self.safety_margin = 0.2 + self.car_width/2
+        self.gap_distance = 2
+        self.range_offset = 180
 
         # TODO: Subscribe to LIDAR
         self.create_subscription(LaserScan, lidarscan_topic, self.lidar_callback, 10)
         # TODO: Publish to drive
+
 
     def preprocess_lidar(self, ranges):
         """ Preprocess the LiDAR scan array. Expert implementation includes:
@@ -27,7 +32,8 @@ class ReactiveFollowGap(Node):
         """
         # Convert to numpy array for easier processing
         proc_ranges = np.array(ranges)
-        
+        # Extract front 180° FOV: combine front-right (270-360°) and front-left (0-90°)
+        proc_ranges = np.array(ranges[self.range_offset:-self.range_offset])
         # Replace inf and nan values with a large number (e.g., 10.0)
         proc_ranges[np.isinf(proc_ranges)] = 10.0
         proc_ranges[np.isnan(proc_ranges)] = 10.0
@@ -42,9 +48,14 @@ class ReactiveFollowGap(Node):
         
         return proc_ranges
 
-    def find_max_gap(self, free_space_ranges):
+    def find_max_gap(self, free_space_ranges) -> tuple :
         """ Return the start index & end index of the max gap in free_space_ranges
         """
+        # threshold for gap distance = self.gap_distance
+        # threshold for gap size >= self.safety_margin
+
+        
+        
         return None
     
     def find_best_point(self, start_i, end_i, ranges):
@@ -52,6 +63,7 @@ class ReactiveFollowGap(Node):
         Return index of best point in ranges
 	    Naive: Choose the furthest point within ranges and go there
         """
+
         return None
 
     def lidar_callback(self, data):
